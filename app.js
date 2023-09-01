f1fbf2b5050041d3c09f9c97e82b28c7ed3286f9
almadance19 <132331786+almadance19@users.noreply.github.com>
2023-09-01 16:53:33 +0200

    commit message

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 //GIT GUIDE
 //git add --all
 //git commit -m "commit message"
+//git commit -m "commit message"git push --force origin
+//git push --force origin master
 
 
 // after loading 
-window.addEventListener('DOMContentLoaded', getData);
+window.addEventListener('DOMContentLoaded', getInit);
+
+function getInit() {
+  //initPayPalButton();
+  getData();
+};
 
 // call API COURSES
-const url = 'https://script.google.com/macros/s/AKfycbw4frixxVaDKqhDgZ3-flTcoUoGKm4l9Ncp6MnOGS45CFSuETrVGH0qoqSYABxYXn_3/exec';
-const urluser =  'https://script.google.com/macros/s/AKfycby2r8RZ0zeuXg15SMNCMIWXstCB_VR-7NU_rA0w0_R3-ixnfCgUFcozGh8bKVBfpQLg/exec';
+const url = 'https://script.google.com/macros/s/AKfycbxzYlcbdFzS463CkGaXu-1vSiWfx0hHGO1zJD4P4sgrUnAOhGWWO_k9Lu4rMj3Q2btV/exec';
+
+const urluser =  'https://script.google.com/macros/s/AKfycbzWu6k32M7XjlK51cEYH-5qqO0Az5cHKI1zypbU_nHwFxzGGP2DmOa4U0u6FbY9AO-r/exec';
 
 //get buttons 
 const output = document.querySelector('.output');
 const btnPayment = document.querySelector('.payment');
 const btnPayments = document.querySelector('.allpayments');
 const btnEmail = document.querySelector('.emailer');
-const btnReload = document.querySelector('.ref');
+const btnPaypal = document.querySelector('.paypal');
 
 //get fields
 //const iName = document.querySelector('input[name=name]');
@@ -25,7 +33,7 @@ const repMessage = document.querySelector('.rep');
 //event listener buttons 
 btnPayment.addEventListener('click', showPaymentModal);
 btnPayments.addEventListener('click', showPaymentsModal);
-btnReload.addEventListener('click', getData);
+btnPaypal.addEventListener('click', paymentForm,initPayPalButton);
 btnEmail.addEventListener('click', getUser);
  
 //get user fields
@@ -211,6 +219,7 @@ function getUser() {
           nextpaymentinput.value = val[4];
           btnPayment.style.display = "block";
           btnPayments.style.display = "block";
+          btnPaypal.style.display = "block";
 
         });
         showPayment(data.activepayment);
@@ -383,6 +392,7 @@ function showPayment(payment)
     };
     
 
+  /// Save Class Registration and Post in Classes Api
     function AddRow(id,name,adress,dia,hora,fecha,details,genre,last_payment,last_due_payment,active)
     {
       var firstname = document.getElementById("firstname").value;
@@ -421,10 +431,7 @@ function sData(arr) {
   repMessage.textContent = "Sending";
   //let val1 = iName.value || 'unknown';
   //let val2 = iMes.value || 'Message';
-  //iName.style.display = 'none';
-  //iMes.style.display = 'none';
   //btnSave.style.display = 'none';
-  //let arr = [val1, val2];
   let formData = new FormData();
   formData.append('data', JSON.stringify(arr));
   console.log("posting registration in API")
@@ -436,6 +443,275 @@ function sData(arr) {
   }).then(function (data) {
      console.log("Subscribed");
     //repMessage.textContent = "Subscribed" ;
-    //getData();
   })
+};
+
+
+
+
+/// CREATE PAYMENT FORM 
+
+
+function paymentForm()
+{
+    var name_user = document.getElementById("User_name").value;
+    var last_payment = document.getElementById("User_lastpayment").value;
+    var last_due_payment = document.getElementById("User_nextpayment").value;
+    var active = document.getElementById("User_active").value;
+    var user_email = document.getElementById("User_email").value;
+
+
+
+    var displayTable = ""
+    displayTable += "<div class=\"modal-body\">";
+    displayTable += "<div class=\"container-fluid\">";
+    displayTable += "<div class=\"row\">";
+    displayTable += "<div class=\"col\" style=\"font-weight: bold\" ></div>";
+
+    displayTable += "</div>";
+
+    var espace = ": ";
+    displayTable += "<div class=\"row\">"+"<strong>Adresse/Address</strong> "+"adress"+"</div>";
+    displayTable += "<div class=\"row\">"+"<strong>Day/Tag </strong> "+espace+"dia"+"</div>";
+    displayTable += "<div class=\"row\">"+"<strong>Time/Uhrzeit </strong> "+espace+"hora"+"</div>";
+    displayTable += "<div class=\"row\">"+"<strong>Date/Datum </strong> "+espace+"fecha"+"</div>";
+
+    displayTable += "<div class=\"row\"> </div>";
+    displayTable += "<div class=\"row\"> </div>";
+    displayTable += '<form style="background-color:white;color:black">';
+    displayTable += '<div class="form-row">';
+    displayTable += "<label for=\"disabledTextInput\" style=\"font-weight: bold\" >"+" "+"</label>";
+    displayTable += '</div>';
+    displayTable += '<div class="form-row">';
+    displayTable += '<label for="firstname" style="font-weight: bold">Name</label>';
+    displayTable += "<input type=\"text\" id=\"firstname\" class=\"form-control\" Value=\""+name_user+"\" >";
+    displayTable += '<small id="nameHelp" class="form-text text-muted" style="color:yellow" >** Check your name is correct.</small>';
+    displayTable += '</div>';
+    displayTable += '<div class="form-row">';
+    displayTable += '<label for="course" style="font-weight: bold">Course(s) / Kurs(e)</label>';
+    displayTable += "<input type=\"text\" id=\"course\" class=\"form-control\" Value=\""+""+"\" >";
+    displayTable += '<small id="nameHelp" class="form-text text-muted" style="color:yellow" >** Check your name is correct.</small>';
+    displayTable += '</div>';
+    displayTable += '<div class="form-row">';
+    displayTable += '<label for="membershiptype" style="font-weight: bold">Membership Type / Mitgliedschaftstyp</label>';
+    displayTable += '<select class="custom-select" id="membershiptype">';
+    displayTable += '<option>1 Month Subscription</option>';
+    displayTable += '<option>3 Months Subscription</option>';
+    displayTable += '<option>6 Months Subscription</option>';
+    displayTable += '<option>12 Months Subscription</option>';
+    displayTable += '<option>4er Karte</option>';
+    displayTable += '<option>10er Karte</option>';
+    displayTable += '</select>';
+    displayTable += '</div>';
+    displayTable += '<div class="form-row">';
+    displayTable += '<label for="coursesnumber" style="font-weight: bold">Membership / Mitgliedschaft</label>';
+    displayTable += '<select class="custom-select" id="coursesnumber">';
+    displayTable += '<option>1 Course/Kurs</option>';
+    displayTable += '<option>2 Courses/Kurse</option>';
+    displayTable += '<option>3 Courses/Kurse</option>';
+    displayTable += '<option>Flatrate</option>';
+    displayTable += '</select>';
+    displayTable += '</div>';
+    displayTable += '<div class="form-row">';
+    displayTable += '<label for="exampleFormControlSelect2" style="font-weight: bold">New Member? / Neue Mitgliedschaft</label>';
+    displayTable += '<select class="custom-select" id="newmember">';
+    displayTable += '<option value="1" selected="selected" >Yes / Ja</option>';
+    displayTable += '<option value="0" >No / Nein</option>';
+    displayTable += '</select>';
+    displayTable += '</div>';
+    displayTable += '<div class="form-row">';
+    displayTable += '<label for="emaiemail_paymentl" style="font-weight: bold" >Email (mandatory/erforderlich) </label>';
+    displayTable += "<input type=\"email\" id=\"email_payment\" class=\"form-control\" aria-describedby=\"emailHelp\" Value=\""+user_email+"\" >";
+    displayTable += '<small id="emailHelp" class="form-text text-muted" style="color:yellow" >** Check your email is correct.</small>';
+    displayTable += '<div id="display_error" style="color: red" ></div>';
+    displayTable += '<div id="display_success" style="color: black" ></div>';
+    displayTable += '</div>';
+    displayTable += '<div class="form-row">';
+    displayTable += '<div id="display_error" style="color: red" ></div>';
+    displayTable += '<div id="display_success" style="color: black" ></div>';
+    displayTable += '</div>';
+    displayTable += '<div class="form-group col-md-3">';
+    displayTable += "<input type=\"button\" value=\"Pay with Paypal\" id=\"addRegis\" class=\"btn btn-colour-1\" ";
+    displayTable += " onclick=\"paypalProcess('"+name_user+"',"+"'"+name_user+"',"+"'"+name_user+"',"+"'"+name_user+"',"+"'"+name_user+"',"+"'"+name_user+"')\" />";
+    displayTable += '</div>';
+    displayTable += '<div class="form-group col-md-3">';
+    displayTable += "<input type=\"button\" value=\"Bank Transfer payment\" id=\"addRegis\" class=\"btn btn-colour-1\" ";
+    displayTable += " onclick=\"bankProcess('"+name_user+"',"+"'"+name_user+"',"+"'"+name_user+"',"+"'"+name_user+"',"+"'"+name_user+"',"+"'"+name_user+"')\" />";
+    displayTable += '</div>';
+    displayTable += "</div>";
+    displayTable += "</div>";
+  
+    $("#pay-form-container").html(displayTable);
+};
+
+
+function paypalProcess() {
+  //get prices from db and select from dictionary
+  //get payment data
+  console.log("Paypal Process starting");
+  document.getElementById('ItemOrdered').value = 'NUEVO PRECIO';
+  document.getElementById('ItemOrdered').setAttribute( "price", "15" );
+  document.getElementById('ItemOrdered').innerHTML = 'NUEVO PRECIO - 25';
+
+  initPayPalButton();
+  //create a new user if new
+  //create a new payment
+  //send a payment 
+  //show a receipt 
+};
+
+function bankProcess() {
+  console.log("Bank Process starting");
 }
+
+
+var dict_preise = {
+  "1 Course/Kurs-1 Month Subscription" : 60 ,
+  "1 Course/Kurs-3 Months Subscription" : 50 ,
+  "1 Course/Kurs-6 Months Subscription" : 45 ,
+  "1 Course/Kurs-12 Months Subscription" : 40 ,
+  "2 Courses/Kurse-1 Month Subscription" : 90 ,
+  "2 Courses/Kurse-3 Months Subscription" : 75 ,
+  "2 Courses/Kurse-6 Months Subscription" : 70 ,
+  "2 Courses/Kurse-12 Months Subscription" : 60 ,
+  "3 Courses/Kurse-1 Month Subscription" : 100 ,
+  "3 Courses/Kurse-3 Months Subscription" : 90 ,
+  "3 Courses/Kurse-6 Months Subscription" : 80 ,
+  "3 Courses/Kurse-12 Months Subscription" : 70 ,
+  "Flatrate-1 Month Subscription" : 110 ,
+  "Flatrate-3 Months Subscription" : 100 ,
+  "Flatrate-6 Months Subscription" : 90 ,
+  "Flatrate-12 Months Subscription" : 80 ,
+  "1 Course/Kurs-4er Karte" : 65 ,
+  "2 Courses/Kurse-4er Karte" : 65 ,
+  "3 Courses/Kurse-4er Karte" : 65 ,
+  "Flatrate-4er Karte" : 65 ,
+  "1 Course/Kurs-10er Karte" : 120 ,
+  "2 Courses/Kurse-10er Karte" : 120 ,
+  "3 Courses/Kurse-10er Karte" : 120 ,
+  "Flatrate-10er Karte" : 120 
+};
+
+
+
+//// PAYPAL 
+
+
+function initPayPalButton() {
+  document.querySelector(".section-2").style.display = 'none'; 
+
+  var shipping = 0;
+  var itemOptions = document.querySelector("#smart-button-container #item-options");
+var quantity = parseInt();
+var quantitySelect = document.querySelector("#smart-button-container #quantitySelect");
+if (!isNaN(quantity)) {
+quantitySelect.style.visibility = "visible";
+}
+var orderDescription = 'FRANKFURT SPRING BACHATHON 2023';
+if(orderDescription === '') {
+orderDescription = 'Item';
+}
+paypal.Buttons({
+style: {
+  shape: 'rect',
+  color: 'gold',
+  layout: 'vertical',
+  label: 'paypal',
+  
+},
+createOrder: function(data, actions) {
+  var selectedItemDescription = itemOptions.options[itemOptions.selectedIndex].value;
+  var selectedItemPrice = parseFloat(itemOptions.options[itemOptions.selectedIndex].getAttribute("price"));
+  var tax = (0 === 0 || false) ? 0 : (selectedItemPrice * (parseFloat(0)/100));
+  if(quantitySelect.options.length > 0) {
+    quantity = parseInt(quantitySelect.options[quantitySelect.selectedIndex].value);
+  } else {
+    quantity = 1;
+  }
+
+  tax *= quantity;
+  tax = Math.round(tax * 100) / 100;
+  var priceTotal = quantity * selectedItemPrice + parseFloat(shipping) + tax;
+  priceTotal = Math.round(priceTotal * 100) / 100;
+  var itemTotalValue = Math.round((selectedItemPrice * quantity) * 100) / 100;
+
+  return actions.order.create({
+    purchase_units: [{
+      description: orderDescription,
+      amount: {
+        currency_code: 'EUR',
+        value: priceTotal,
+        breakdown: {
+          item_total: {
+            currency_code: 'EUR',
+            value: itemTotalValue,
+          },
+          shipping: {
+            currency_code: 'EUR',
+            value: shipping,
+          },
+          tax_total: {
+            currency_code: 'EUR',
+            value: tax,
+          }
+        }
+      },
+      items: [{
+        name: selectedItemDescription,
+        unit_amount: {
+          currency_code: 'EUR',
+          value: selectedItemPrice,
+        },
+        quantity: quantity
+      }]
+    }]
+  });
+},
+onApprove: function(data, actions) {
+  return actions.order.capture().then(function(orderData) {
+    
+    // Full available details
+    console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+
+    // Show a success message within this page, e.g.
+    const element = document.getElementById('paypal-button-container');
+    element.innerHTML = '';
+    element.innerHTML = '<h3>Thank you for your payment!</h3>';
+    document.getElementById('smart-button-container').style.display = 'none';
+    console.log('This person paid',emailinput.value, ", ",nameinput.value, ", ",idinput.value );
+    document.querySelector(".section-2").style.display = 'block'; 
+    let arr_pay = [emailinput.value,nameinput.value,idinput.value];
+    console.log(arr_pay);
+    sDataPay(arr_pay);
+
+    // Or go to another URL:  actions.redirect('thank_you.html');
+
+  });
+},
+onError: function(err) {
+  console.log(err);
+},
+}).render('#paypal-button-container');
+}
+
+
+    // Save Booking hl
+    function sDataPay(arr) {
+      //e.preventDefault();
+       repMessage.textContent = "Sending";
+       //let val1 = iName.value || 'unknown';
+       //let val2 = iMes.value || 'Message';
+       //btnSave.style.display = 'none';
+       let formData = new FormData();
+       formData.append('data', JSON.stringify(arr));
+       console.log("posting registration in API")
+       fetch(urluser, {
+         method: 'POST'
+         , body: formData
+       }).then(function (rep) {
+         return rep.json()
+       }).then(function (data) {
+          console.log("Subscribed");
+         //repMessage.textContent = "Subscribed" ;
+       })
+     };
