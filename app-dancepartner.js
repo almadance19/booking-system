694751871a9e3195f1a320451a3e096e6265b50b
almadance19 <132331786+almadance19@users.noreply.github.com>
2023-11-09 18:15:48 +0100

    commit message

--- app-dancepartner.js
+++ app-dancepartner.js
@@ -23,7 +23,7 @@ window.onload = (event) => {
         console.log(extractedUser);
         if (extractedUser) {
           console.log('Got the id - ' + extractedUser.id);
-          
+
           emailinput.disabled = false;
           nameinput.disabled = false;
           coursesinput.disabled = false;
@@ -36,12 +36,14 @@ window.onload = (event) => {
 
           coursesinput.value=extractedUser.name;
           emailinput.value=extractedUser.email;
-          nameinput.value = extractedUser.name;
+          nameinput.value = extractedUser.level;
+          activeinput.value = extractedUser.gender;
           idinput.value = extractedUser.id;
           lastpaymentinput.value = extractedUser.styles;
           saldoinput.value = extractedUser.ideal_partner;
           anmerkungeninput.value = extractedUser.goals;
           nextpaymentinput.value = extractedUser.description;
+          document.querySelector('input[name=email-class]').value= extractedUser.email;
   
         } else {
           console.log('Could not find id.');
@@ -55,8 +57,9 @@ window.onload = (event) => {
 function getLogin() { 
     var email_value = document.querySelector('input[name=email-class]').value;
     if (email_value != '') { 
-        output.innerHTML = "..loading";
-        getUser(email_value);
+        output.innerHTML = "..posting new data";
+        /// SEND EDITED INFORMATION TO PROFILE
+        //getUser(email_value);
         
       } else {
         output.innerHTML = "Enter a valid email";
@@ -67,48 +70,8 @@ function getLogin() {
 
 // get users data 
 function getUser(email_value) {
-    if (email_value != '') {
-      urlapi = urluser+"/exec?code="+email_value
-      console.log(urlapi);
-      //output.innerHTML = "loading...";
-      console.log("fetching user data");
-      fetch(urlapi).then(function (rep) {
-        return rep.json()
-      }).then(function (val) {
-        console.log(val);
-        output.innerHTML = "";
-          var name_user = val.posts[0][1] 
-          console.log(name_user);
-          if (val.posts[0][1]  == 'No Active User') {
-          output.innerHTML = "Email: "+email_value+" not registered yet. Register!";
-          } 
-          else {
-          activeinput.value = val.posts[0][2];
 
-          emailinput.disabled = false;
-          nameinput.disabled = false;
-          coursesinput.disabled = false;
-          idinput.disabled = false;
-          lastpaymentinput.disabled = false;
-          saldoinput.disabled = false;
-          anmerkungeninput.disabled = false;
-          nextpaymentinput.disabled = false;
 
-          emailinput.value = email_value;
-          nameinput.value = val.posts[0][3];
-          coursesinput.value = val.posts[0][1];
-          idinput.value = val.posts[0][0];
-          lastpaymentinput.value = val.posts[0][7];
-          saldoinput.value = val.posts[0][5];
-          anmerkungeninput.value = val.posts[0][6];
-          nextpaymentinput.value = val.posts[0][4];
-
-        }
-        });
-
-      } else {
-        output.innerHTML = "Enter a valid email";
-      }
 }
 
 //get user fields
